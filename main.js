b702f4fba1f8304fd9c840892c63071508a01ab8
Geovani Rodrigues <geovanni3344@outlook.com>
2026-03-11 21:15:58 -0300

    Ajuste no CSS e melhorias nas responsividade.

--- main.js
+++ main.js
@@ -1,22 +1,23 @@
-$(document).ready(function () {
+$(document).ready(function () { // Garantir que o DOM esteja carregado antes de manipular os elementos
+    // Selecionar os elementos do envelope e dos botões
   var envelope = $("#envelope");
   var btn_open = $("#open");
   var btn_reset = $("#reset");
 
-  envelope.click(function () {
+  envelope.click(function () { // Adicionar um evento de clique ao envelope para abrir ou fechar a carta
     open();
   });
-  btn_open.click(function () {
+  btn_open.click(function () { // Adicionar um evento de clique ao botão "abrir"para abrir a carta
     open();
   });
-  btn_reset.click(function () {
+  btn_reset.click(function () { // Adicionar um evento de clique ao botão reset para fechar a carta
     close();
   });
 
-  function open() {
+  function open() { // Função para abrir a carta, adicionando a classe "open" e removendo a classe "close"
     envelope.addClass("open").removeClass("close");
   }
-  function close() {
+  function close() {  // Função para fechar a carta, adicionando a classe "close" e removendo a classe "open"
     envelope.addClass("close").removeClass("open");
   }
 });
